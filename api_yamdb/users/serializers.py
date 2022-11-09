6258833b367ab4eb3fadc721f1d4653d1e238c1a
Ильшат Кушманбетов <ilshat261093@yandex.ru>
2022-11-09 14:56:48 +0600

    1.1

---
+++ api_yamdb/users/serializers.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+FIELDS = (
+    'username',
+    'email',
+    'first_name',
+    'last_name',
+    'bio',
+    'role'
+)
+
+
+class UserSignUpSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    confirmation_code = serializers.CharField()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = FIELDS
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError("Choose another name")
+        return value
+
+
+class UserSelfSerializer(UserSerializer):
+    class Meta:
+        model = User
+        fields = FIELDS
+        read_only_fields = ('role',)
